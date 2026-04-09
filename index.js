533246a475f1b4f67ddae0820c0223569d3d9ebf
lindamarani-source <linda.marani@student.moringaschool.com>
2026-04-09 11:39:01 +0300

    First Commit

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-
-
+function createLoginTracker(username, info) {
+  const userInfo = {
+    username: username,
+    info: info
+  };
+  attemptCount = 0;
+  return {
+    passwordAttempt: () =>{
+      announce = 'Login Successful';
+      if (attemptCount < 3) {
+        attemptCount++;
+        announce = 'Login Failed';
+      }
+    }
+}
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
